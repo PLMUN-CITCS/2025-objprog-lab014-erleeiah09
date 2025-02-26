6f1c47e4dc086690110e42313788e38102155bee
erleeiah09 <jaloconerleeiah_bscs@plmun.edu.ph>
2025-02-26 14:10:08 +0800

    Last Sync: 2025-02-26 06:10:08 (Mobile)

---
+++ MethodReturnTypes.java
@@ -0,0 +1,27 @@
+public class MethodReturnTypes {
+
+    // Method to display a welcome message
+    public static void displayWelcomeMessage() {
+        System.out.println("Welcome to our program!");
+    }
+
+    // Method to calculate the average of two numbers
+    public static double calculateAverage(int num1, int num2) {
+        double average = (num1 + num2) / 2.0;
+        return average;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        displayWelcomeMessage(); // Calling the welcome message method
+
+        int value1 = 20;
+        int value2 = 30;
+
+        // Calling calculateAverage and storing result
+        double result = calculateAverage(value1, value2);
+
+        // Printing the result
+        System.out.println("The average is: " + result);
+    }
+}
